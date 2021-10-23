49f7f937e7c37e46737562c6034fa8bceaa3cbcf
jake lamers <jakelamers931@gmail.com>
2021-10-23 13:49:24 -0500

    Finished up through getting stored data from shared preferences

--- app/src/main/java/com/example/lab5/MainActivity.java
+++ app/src/main/java/com/example/lab5/MainActivity.java
@@ -2,17 +2,27 @@ package com.example.lab5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        SharedPreferences sharedPreferences = getSharedPreferences("com.example.lab5", Context.MODE_PRIVATE);
+        String username = sharedPreferences.getString("username", "");
+
+        if (!username.equals("")) {
+            Intent intent = new Intent(this,MainActivity2.class);
+            intent.putExtra("message",String.valueOf(username));
+            startActivity(intent);
+        } else {
+            setContentView(R.layout.activity_main);
+        }
     }
 
     public void onLogin(View view){
@@ -20,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
         EditText Password = (EditText) findViewById(R.id.Password);
 
         if (Password.getText().toString().length() > 0){
+            SharedPreferences sharedPreferences = getSharedPreferences("com.example.lab5", Context.MODE_PRIVATE);
+            sharedPreferences.edit().putString("username", Username.getText().toString()).apply();
             goToActivity2(Username.getText().toString());
         }
     }
